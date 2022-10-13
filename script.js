f7937b9e4702c83ab5fbd805e7b32173b9d69f9e
Alex <sponge.leha@gmail.com>
2022-10-13 17:14:52 +0300

    spin added; attempted to make responsive (again)

--- script.js
+++ script.js
@@ -9,6 +9,38 @@ window.addEventListener('resize', (event) => {
     let SIZE = document.querySelector(".wrapper").offsetWidth;
     drawWheel(SIZE, SECTOR_COUNT);
 });
+let spinBtn = document.getElementById("daspin");
+let winnerText = document.getElementById('winner');
+spinBtn.addEventListener("click", (event) => {
+    winnerText.textContent = "DECIDE THE WINNER";
+    winnerText.classList.remove("linear-wipe");
+    winnerText.classList.add("deciding");
+    spinBtn.classList.add("btn-disappear");
+    input.setAttribute('disabled', true);
+    let dawheel = document.getElementById("wheel-svg");
+    document.documentElement.style.setProperty('--random-degree', 
+    `${360 * 10 + Math.floor(Math.random() * 10) * 360 + Math.floor(Math.random() * 361)}deg`);
+    dawheel.classList.remove("idling");
+    dawheel.classList.add("spin");
+    setTimeout(() => {
+        const arrow = document.querySelector(".arrow");
+        const arrowCoords = arrow.getBoundingClientRect();
+        const pointingCoords = [arrowCoords.left + (arrowCoords.right - arrowCoords.left) / 2, 
+                                arrowCoords.top + (arrowCoords.bottom - arrowCoords.top) / 2];
+        console.log("pointing at ", pointingCoords);
+        let element = document.elementsFromPoint(...pointingCoords).find(el => el.id.startsWith('sector-'));
+        console.log("found element: ", element);
+        winnerText.textContent = `WINNER IS ${element.getAttribute("fill").toUpperCase()}`;
+        winnerText.classList.remove("deciding");
+        winnerText.classList.add("linear-wipe");
+    }, 30 * 1000 + 100);
+    setTimeout(() => {
+        spinBtn.classList.remove("btn-disappear");
+        input.removeAttribute('disabled');
+        dawheel.classList.remove("spin");
+        dawheel.classList.add("idling");
+    }, 35 * 1000);
+});
 const TO_RADIAN = Math.PI / 180;
 const COLORS = [
     "red",
@@ -29,7 +61,8 @@ const COLORS = [
     "plum",
     "tan",
     "whitesmoke",
-    "turquoise"
+    "turquoise",
+    "gray"
 ];
 
 drawWheel(SIZE, SECTOR_COUNT);
@@ -42,6 +75,7 @@ function drawWheel(size, count = 1) {
     // Create the svg element and a default gray circle
     let container = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     container.setAttribute('id','wheel-svg');
+    container.classList.add("idling");
     let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
 
     const radius = size / 2;
@@ -92,6 +126,7 @@ function drawWheel(size, count = 1) {
         let sector = document.createElementNS('http://www.w3.org/2000/svg', 'path');
         sector.setAttribute('d', path);
         sector.setAttribute('fill', `${COLORS[i]}`);
+        sector.setAttribute('id', `sector-${i}`);
         sector.style.transformOrigin = `${radius}px ${radius}px`;
         sector.style.transform = `rotate(${360 / count * i}deg)`;
         container.appendChild(sector);
