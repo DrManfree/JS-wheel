9d0288ae2d1cc64452f96e7fbdfd9407fe06b861
Alex <sponge.leha@gmail.com>
2022-10-13 19:30:25 +0300

    fixed random; added text to sectors

--- script.js
+++ script.js
@@ -1,47 +1,5 @@
-let SIZE = document.querySelector(".wrapper").offsetWidth;
-let SECTOR_COUNT = 4;
-let input = document.getElementById("input-sectors");
-input.addEventListener("change", (event) => {
-    SECTOR_COUNT = +event.target.value;
-    drawWheel(SIZE, SECTOR_COUNT);
-});
-window.addEventListener('resize', (event) => {
-    let SIZE = document.querySelector(".wrapper").offsetWidth;
-    drawWheel(SIZE, SECTOR_COUNT);
-});
-let spinBtn = document.getElementById("daspin");
-let winnerText = document.getElementById('winner');
-spinBtn.addEventListener("click", (event) => {
-    winnerText.textContent = "DECIDE THE WINNER";
-    winnerText.classList.remove("linear-wipe");
-    winnerText.classList.add("deciding");
-    spinBtn.classList.add("btn-disappear");
-    input.setAttribute('disabled', true);
-    let dawheel = document.getElementById("wheel-svg");
-    document.documentElement.style.setProperty('--random-degree', 
-    `${360 * 10 + Math.floor(Math.random() * 10) * 360 + Math.floor(Math.random() * 361)}deg`);
-    dawheel.classList.remove("idling");
-    dawheel.classList.add("spin");
-    setTimeout(() => {
-        const arrow = document.querySelector(".arrow");
-        const arrowCoords = arrow.getBoundingClientRect();
-        const pointingCoords = [arrowCoords.left + (arrowCoords.right - arrowCoords.left) / 2, 
-                                arrowCoords.top + (arrowCoords.bottom - arrowCoords.top) / 2];
-        console.log("pointing at ", pointingCoords);
-        let element = document.elementsFromPoint(...pointingCoords).find(el => el.id.startsWith('sector-'));
-        console.log("found element: ", element);
-        winnerText.textContent = `WINNER IS ${element.getAttribute("fill").toUpperCase()}`;
-        winnerText.classList.remove("deciding");
-        winnerText.classList.add("linear-wipe");
-    }, 30 * 1000 + 100);
-    setTimeout(() => {
-        spinBtn.classList.remove("btn-disappear");
-        input.removeAttribute('disabled');
-        dawheel.classList.remove("spin");
-        dawheel.classList.add("idling");
-    }, 35 * 1000);
-});
 const TO_RADIAN = Math.PI / 180;
+const SPIN_TIME = 30;
 const COLORS = [
     "red",
     "blue",
@@ -65,6 +23,25 @@ const COLORS = [
     "gray"
 ];
 
+let SIZE = document.querySelector(".wrapper").offsetWidth;
+let input = document.getElementById("input-sectors");
+let SECTOR_COUNT = input.value;
+let spinBtn = document.getElementById("daspin");
+let winnerText = document.getElementById('winner');
+
+input.addEventListener("change", (event) => {
+    SECTOR_COUNT = +event.target.value;
+    drawWheel(SIZE, SECTOR_COUNT);
+});
+window.addEventListener('resize', (event) => {
+    let SIZE = document.querySelector(".wrapper").offsetWidth;
+    drawWheel(SIZE, SECTOR_COUNT);
+});
+spinBtn.addEventListener("click", (event) => {
+    spinWheel();
+});
+
+document.documentElement.style.setProperty('--spin-time', `${SPIN_TIME}s`);
 drawWheel(SIZE, SECTOR_COUNT);
 
 function drawWheel(size, count = 1) {
@@ -90,13 +67,22 @@ function drawWheel(size, count = 1) {
     // If there is only one sector just create a red circle
     if (count === 1) {
         let sector = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-
-        const radius = size / 2;
+        let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');;
+        text.textContent = COLORS[0];
+        text.setAttribute('x', `${radius}`);
+        text.setAttribute('y', `${radius * 0.3}`);
+        text.classList.add('sector-text');
+        text.style.transformOrigin = `${radius}px ${radius}px`;
+        text.style.transform = `rotateZ(${-90 + 360 / count * 0.5}deg)`;
+        text.style.transform += ` rotate(${360 / count}deg)`;
+        document.documentElement.style.setProperty('--sector-text-size', `${Math.max(3 * 2 / count, 1)}em`);
+        text.style.transform += ` translate(${radius * 0.3}px, ${radius * 0.71}px)`;
         circle.setAttribute('cx', `${radius}`);
         circle.setAttribute('cy', `${radius}`);
         circle.setAttribute('r', `${radius}`);
         circle.setAttribute('fill', `red`);
         container.appendChild(circle);
+        container.appendChild(text);
         return;
     }
 
@@ -124,11 +110,65 @@ function drawWheel(size, count = 1) {
     // Create each sector, set its color, rotate it, and append it to the wheel
     for (let i = 0; i < count; i++) {
         let sector = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');;
+        text.textContent = COLORS[i];
+        text.setAttribute('x', `${radius}`);
+        text.setAttribute('y', `${radius * 0.3}`);
+        text.classList.add('sector-text');
+        text.style.transformOrigin = `${radius}px ${radius}px`;
+        text.style.transform = `rotateZ(${-90 + 360 / count * 0.5}deg)`;
+        text.style.transform += ` rotate(${360 / count * i}deg)`;
+        document.documentElement.style.setProperty('--sector-text-size', `${Math.max(3 * 2 / count, 1)}em`);
+        text.style.transform += ` translate(${radius * 0.3}px, ${radius * 0.71}px)`;
         sector.setAttribute('d', path);
         sector.setAttribute('fill', `${COLORS[i]}`);
-        sector.setAttribute('id', `sector-${i}`);
+        sector.setAttribute('id', `sector-${i + 1}`);
         sector.style.transformOrigin = `${radius}px ${radius}px`;
         sector.style.transform = `rotate(${360 / count * i}deg)`;
         container.appendChild(sector);
+        container.appendChild(text);
     }
+}
+
+function spinWheel() {
+    // Clean-up
+    winnerText.textContent = "DECIDE THE WINNER";
+    winnerText.classList.remove("linear-wipe");
+    winnerText.classList.add("deciding");
+    spinBtn.classList.add("btn-disappear");
+    input.setAttribute('disabled', true);
+
+    let dawheel = document.getElementById("wheel-svg");
+    
+    let additionalSpins = Math.floor(Math.random() * 10);
+    let winningSector = Math.floor(Math.random() * SECTOR_COUNT) + 1;
+    let angle = 360 / SECTOR_COUNT;
+    let additionalDegree = angle * (winningSector - 1) + Math.floor(Math.random() * angle);
+    document.documentElement.style.setProperty('--random-degree', 
+    `${-1 * (360 * 10 + additionalSpins * 360 + additionalDegree)}deg`);
+    dawheel.classList.remove("idling");
+    dawheel.classList.add("spin");
+
+    // Get the sector the arrow is pointing to at the end of the spin and declare it as winner
+    setTimeout(() => {
+        const arrow = document.querySelector(".arrow");
+        const arrowCoords = arrow.getBoundingClientRect();
+        const pointingCoords = [arrowCoords.left + (arrowCoords.right - arrowCoords.left) / 2, 
+                                arrowCoords.top + (arrowCoords.bottom - arrowCoords.top) / 2];
+        let element = document.elementsFromPoint(...pointingCoords).find(el => el.id.startsWith('sector-'));
+        console.log("Winning sector is", winningSector, "found element", element.id);
+        console.log("sector angle is", angle, "span additional", additionalDegree, "from 0");
+        console.log("span total",(360 * 10 + additionalSpins * 360 + additionalDegree), "degrees");
+        winnerText.textContent = `WINNER IS ${element.getAttribute("fill").toUpperCase()}`;
+        winnerText.classList.remove("deciding");
+        winnerText.classList.add("linear-wipe");
+    }, SPIN_TIME * 1000 + 100);
+
+    // Prepare and clean up for next spin
+    setTimeout(() => {
+        spinBtn.classList.remove("btn-disappear");
+        input.removeAttribute('disabled');
+        dawheel.classList.remove("spin");
+        dawheel.classList.add("idling");
+    }, (SPIN_TIME + 5) * 1000);
 }
